ed6c89b926c1b7d71eb914dfc3a24d556be9251e
Gio Cruz <giogodo@gmail.com>
2019-10-16 12:31:42 -0600

    components dir added, Book component started

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
+import { Book } from './components'
 
 class BooksApp extends React.Component {
   state = {
@@ -48,6 +49,13 @@ class BooksApp extends React.Component {
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
+                      <li>
+                        <Book book={{
+                          title: 'To Kill a Mockingbird',
+                          authors: 'Harper Lee',
+                          imageLinks: {thumbnail: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"}
+                        }}/>
+                      </li>
                       <li>
                         <div className="book">
                           <div className="book-top">
---
+++ src/components/Book.js
@@ -0,0 +1,51 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class Book extends React.Component {
+  render() {
+    const { book } = this.props;
+    const width = 128;
+    const height = 193;
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: width,
+              height: height,
+              backgroundImage:
+                book.imageLinks && book.imageLinks.thumbnail
+                  ? `url(${book.imageLinks.thumbnail})`
+                  : `url(https://dummyimage.com/128x193/000/ffffff.jpg&text=No+image)`
+            }}
+          />
+          <div className="book-shelf-changer">
+            <select>
+              <option value="move" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors}</div>
+      </div>
+    );
+  }
+}
+
+Book.propTypes = {
+  book: PropTypes.object
+}
+
+Book.defaultProps = {
+  book: {
+    title: 'none',
+    authors: 'none'
+  }
+}
+
+export default Book;
